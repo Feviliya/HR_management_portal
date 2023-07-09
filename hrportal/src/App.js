a0289fbc0cbe7f25955bdf8212f46e5bc1b79934
Feviliya J <feviliya28@gmail.com>
2023-07-09 17:32:59 +0530

    second commit

--- hrportal/src/App.js
+++ hrportal/src/App.js
@@ -1,11 +1,20 @@
 
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
 import './App.css';
-
+import Landing from './pages/landing';
+import AdminMain from './pages/admin/adminMain';
+import EmployeeMain from './pages/employee/employeeMain';
+import EmployeeDash from './pages/employee/employeeDash';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Landing></Landing>}/>
+      <Route path='/employee' element={<EmployeeMain></EmployeeMain>}/>;
+      <Route path='/admin' element={<AdminMain></AdminMain>}/>;
+      <Route path='/employee/dashboard' element={<EmployeeDash></EmployeeDash>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ hrportal/src/pages/employee/employeeDash.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EmployeeDash() {
+  return (
+    <div>
+        dashboard
+    </div>
+  )
+}
+
+export default EmployeeDash
