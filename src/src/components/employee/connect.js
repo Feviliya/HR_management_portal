bba4bf11f416a741fab2ed5d9fcc32da4687e9d4
Feviliya J <93245596+Feviliya@users.noreply.github.com>
2023-07-25 22:01:05 +0530

    Delete src directory

--- src/src/components/employee/connect.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import '../../assets/css-components/employeecss/connect.css'
-import ChatsPage from '../../pages/employee/EmpDashBoard-contents/chatsPage'
-const Connect = () => {
-        return (<ChatsPage/>);
-
-}
-
-export default Connect
